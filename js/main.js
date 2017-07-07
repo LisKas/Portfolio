85dded52a72ffde2bace5d009f5d5e1f169122b9
Alice Kasyanova <liskas87@mail.ru>
2017-07-07 16:43:04 +0300

    changed hover

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
 
-    $(".list-site li").click(function(){
-        $(this).children(".nav").slideToggle("slow");
-         return false;
-                   
-    });
-
-    $(".contact").click(function(){
+   $(".contact").click(function(){
        $(this).children(".text").fadeToggle("slow");
                     
         $(this).toggleClass("active");
